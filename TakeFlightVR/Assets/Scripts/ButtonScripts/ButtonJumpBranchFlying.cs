37df98a76c83f8a0660262a683551d5a80c1cee4
simran bhatia <simran18@uw.edu>
2020-02-19 13:41:05 -0800

    Add button scripts

---
+++ TakeFlightVR/Assets/Scripts/ButtonScripts/ButtonJumpBranchFlying.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonJumpBranchFlying: MonoBehaviour
+{
+    public Button buttonJumpBranchFlying;
+    // Use this for initialization
+    void Start()
+    {
+        Button btn= buttonJumpBranchFlying.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick)
+    }
+
+    // Update is called once per frame
+    void TaskOnClick()
+    {
+        // triggers animation
+        Debug.log("branch jump to flying animation")
+    }
+}
---
+++ TakeFlightVR/Assets/Scripts/ButtonScripts/ButtonThrowHelmet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonThrowHelmet : MonoBehaviour
+{
+    public Button buttonThrowHelmet;
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = buttonThrowHelmet.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick)
+    }
+
+    // Update is called once per frame
+    void TaskOnClick()
+    {
+        // triggers animation
+        Debug.log("helmet thrown")
+    }
+}
