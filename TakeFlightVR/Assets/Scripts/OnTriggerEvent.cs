c10caa9359b395de1000e2470a0c637dc969479d
Terrell Strong <stront2@uw.edu>
2020-02-20 14:57:17 -0800

    flying ring prefab and triggerEvent script added

---
+++ TakeFlightVR/Assets/Scripts/OnTriggerEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class OnTriggerEvent : MonoBehaviour
+{
+    public UnityEvent onTriggerEnterEvent;
+    public UnityEvent onTriggerExitEvent;
+
+    private void OnTriggerEnter(Collider other) {
+        if (onTriggerEnterEvent != null) {
+            onTriggerEnterEvent.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (onTriggerExitEvent != null) {
+            onTriggerExitEvent.Invoke();
+        }
+    }
+}
