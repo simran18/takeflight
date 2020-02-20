5e309ace4d5dd4ef41ea2b185277e256c8e6dc98
alicelihh <alicehh@uw.edu>
2020-02-20 15:48:57 -0800

    Combine EnlargeBook & MomWalksInside, and add some musics

---
+++ TakeFlightVR/Assets/Scripts/Coroutines/TextMeshFadeCoroutine.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextMeshFadeCoroutine
+{
+    public static IEnumerator Fade(TextMesh mesh, float fromAlpha, float toAlpha, float duration)
+    {
+        var elapsedTime = 0f;
+        mesh.color = new Color(mesh.color.r, mesh.color.g, mesh.color.b, fromAlpha);
+        var startingColor = mesh.color;
+        var targetColor = new Color(mesh.color.r, mesh.color.g, mesh.color.b, toAlpha);
+        while (elapsedTime < duration)
+        {
+            var timeRatio = elapsedTime / duration;
+            mesh.color = Color.Lerp(startingColor, targetColor, timeRatio);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        mesh.color = targetColor;
+    }
+}
---
+++ TakeFlightVR/Assets/Scripts/LogicBranches/CallbackBranches.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CallbackBranches : MonoBehaviour
+{
+    public delegate void OnCallEndHandler(string nextCall);
+
+    public Callable[] branches;
+    public Callable startPoint;
+
+    private Dictionary<string, Callable> branchDict;
+    private Callable nextCall;
+
+    public event OnCallEndHandler OnCallEnd;
+
+    public CallbackBranches()
+    {
+        this.OnCallEnd = HandleOnCallEnd;
+    }
+
+    void Start()
+    {
+        branches = branches == null ? new Callable[0] : branches;
+        branchDict = new Dictionary<string, Callable>(branches.Length);
+        foreach (Callable c in branches)
+        {
+            branchDict[c.Name] = c;
+        }
+        nextCall = startPoint;
+    }
+
+    private void Update()
+    {
+        if (nextCall != null)
+        {
+            nextCall.Call(this.OnCallEnd);
+            nextCall = null;
+        }
+    }
+
+    private void HandleOnCallEnd(string nextCall)
+    {
+        Debug.Assert(branchDict.ContainsKey(nextCall));
+        this.nextCall = branchDict[nextCall];
+    }
+}
