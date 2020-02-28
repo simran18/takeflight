9fb054cb14f8a84524e0e72b59f4ab56bdc978fb
Owen Wang <owenwangwyx@gmail.com>
2020-02-27 19:11:20 -0800

    Provide a way to use keyboard to debug instead of the Oculus Controller.

--- TakeFlightVR/Assets/Scripts/Coroutines/OvrButtonCoroutine.cs
+++ TakeFlightVR/Assets/Scripts/Coroutines/OvrButtonCoroutine.cs
@@ -3,11 +3,30 @@ using UnityEngine.Events;
 using System.Collections;
 
 
+#if DEBUG
+using System.Collections.Generic;
+#endif
+
+
 [System.Serializable]
 public class OVRButtonEvent : UnityEvent<OVRInput.Button> { }
 
 public static class OVRButtonCoroutine
 {
+#if DEBUG
+    private static readonly Dictionary<OVRInput.Button, KeyCode> KEYBOARD_MAPPING = new Dictionary<OVRInput.Button, KeyCode>()
+    {
+        { OVRInput.Button.One, KeyCode.Q },
+        { OVRInput.Button.Two, KeyCode.W },
+        { OVRInput.Button.Three, KeyCode.E },
+        { OVRInput.Button.Four, KeyCode.R },
+        { OVRInput.Button.PrimaryIndexTrigger, KeyCode.RightBracket },
+        { OVRInput.Button.SecondaryIndexTrigger, KeyCode.LeftBracket },
+        { OVRInput.Button.PrimaryHandTrigger, KeyCode.Quote },
+        { OVRInput.Button.SecondaryHandTrigger, KeyCode.Colon },
+    };
+#endif
+
     // If you need to do something after the given button down, use "yield return WaitForButtonDown(...)";
     // otherwise, you can ignore the returned value.
     public static IEnumerator WaitForButtonDown(OVRInput.Button button, OVRButtonEvent callback, float timeout = -1)
@@ -21,6 +40,13 @@ public static class OVRButtonCoroutine
                 callback.Invoke(button);
                 break;
             }
+#if DEBUG
+            if (KEYBOARD_MAPPING.TryGetValue(button, out var key) && Input.GetKeyDown(key))
+            {
+                callback.Invoke(button);
+                break;
+            }
+#endif
             yield return new WaitForFixedUpdate();
         }
     }
@@ -40,6 +66,13 @@ public static class OVRButtonCoroutine
                     callback.Invoke(button);
                     break;
                 }
+#if DEBUG
+                if (KEYBOARD_MAPPING.TryGetValue(button, out var key) && Input.GetKeyDown(key))
+                {
+                    callback.Invoke(button);
+                    break;
+                }
+#endif
             }
             yield return new WaitForFixedUpdate();
         }
