8e0232b773fcac66485db037ecfd76528c61c239
Owen Wang <owenwangwyx@gmail.com>
2020-02-27 19:08:08 -0800

    Remove unused fields and methods.

--- TakeFlightVR/Assets/Scripts/LogicBranches/LogicBranch.cs
+++ TakeFlightVR/Assets/Scripts/LogicBranches/LogicBranch.cs
@@ -13,35 +13,9 @@ public abstract class LogicBranch : MonoBehaviour
     public bool disableOnLeave = true;
     public bool autoRegisterSelf = true;
 
-    # region inspector workflow changes
-    // Working on improving the inspector interface of Callable - 
-    [Header("Unity Events")]
+    [Header("Events")]
     public UnityEvent<string> OnCallEvent;
     public UnityEvent<string> OnCallEndEvent;
-
-    [Header("Triggers + Transitions")]
-    [SerializeField] private CallTransition[] transitions = new CallTransition[0];
-    
-
-    // This class is just used as a way for transitions to be access via events
-    [System.Serializable]
-    private class CallTransition
-    {
-        public string nextCallName;
-    }
-    
-    // num - The index of 'transitions' to trigger the transition for
-    // End the current call and transitons to the next
-    public void TriggerTransition(int num){
-        TriggerTransition(transitions[num].nextCallName);
-    }
-
-    // nextCallName - The name of the next call to make
-    // End the current call and transitons to the next
-    public void TriggerTransition(string nextCallName) {
-        MoveToBranch(nextCallName);
-    }
-    # endregion
 
     public abstract string Name { get; }
 
