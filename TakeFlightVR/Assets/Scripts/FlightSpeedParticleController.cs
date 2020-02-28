60a030ddbf7ee43c98fc083fe28fec25ef4a6879
Terrell Strong <stront2@uw.edu>
2020-02-27 17:06:36 -0800

    work on flyinging mechanic + added hand models

--- TakeFlightVR/Assets/Scripts/FlightSpeedParticleController.cs
+++ TakeFlightVR/Assets/Scripts/FlightSpeedParticleController.cs
@@ -7,24 +7,43 @@ public class FlightSpeedParticleController : MonoBehaviour
 
     public Rigidbody rb;
     public ParticleSystem ps;
+    public Vector3 orbitalSpeedMultipliers = Vector3.one;
+    public float particleSpeedMultiplier = 1;
+
+    private FlightController flightController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        flightController = GetComponentInParent<FlightController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (flightController.flightEnabled && !ps.isPlaying) {
+            ps.Play();
+        } else if (!flightController.flightEnabled && ps.isPlaying) {
+            ps.Stop();
+        }
+
         transform.rotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
         CurveParticles();
+        SetParticleSpeed();
     }
 
+    // Curves the path of the particles based on which direction the rigidbody is moving and rotating
     void CurveParticles() {
-        var vel = ps.velocityOverLifetime;
-        vel.orbitalX = rb.angularVelocity.x;
-        vel.orbitalY = -rb.angularVelocity.y;
-        vel.orbitalZ = rb.angularVelocity.z;
+        var velocityModule = ps.velocityOverLifetime;
+
+        velocityModule.orbitalX = orbitalSpeedMultipliers.x * rb.velocity.y;
+        velocityModule.orbitalY = orbitalSpeedMultipliers.x * rb.angularVelocity.y;
+        velocityModule.orbitalZ = orbitalSpeedMultipliers.x * rb.angularVelocity.y;
+    }
+
+    void SetParticleSpeed() {
+        var velocityModule = ps.velocityOverLifetime;
+
+        velocityModule.z = particleSpeedMultiplier * -(rb.velocity.magnitude) + -30;
     }
 }
